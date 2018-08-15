acf336aecc0071f55aaf7361752cacc3b28b88f4
Matheus Andrade <matheusgandrade@poli.ufrj.br>
2018-08-14 21:36:44 -0300

    grasp example

---
+++ solver/testing_script.py
@@ -0,0 +1,21 @@
+import sys
+
+import graph_tool.all as gt
+
+from solution import Solution, Neighbourhood
+
+f_p = sys.argv[1]
+sol = Solution(40, f_p)
+# print(sol.vertex_impact_ratio_on_tree())
+
+from grasp import grasp
+opt = grasp(Solution, [40, f_p], Neighbourhood)
+# print(sol.g.vp.is_upgraded.a)
+
+# graph = utils.load_instance(f_p)
+# print(graph.num_vertices())
+# tree_map = graph.new_edge_property("bool")
+
+# gt.min_spanning_tree(graph, weights=graph.ep.weight, tree_map=tree_map)
+
+# print(tree_map)
