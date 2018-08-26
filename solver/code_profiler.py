4a40ba84cb0e5c4d2b4232347557276a96c13b07
Matheus Andrade <matheusgandrade@poli.ufrj.br>
2018-08-26 16:18:44 -0300

    keep track of times for initialization and grasp execution

--- solver/code_profiler.py
+++ solver/code_profiler.py
@@ -1,5 +1,6 @@
 import sys
 import cProfile
+import time
 
 import graph_tool.all as gt
 
@@ -7,16 +8,22 @@ from solution import Solution, Neighbourhood
 from utils import load_instance
 from grasp import grasp
 
+times = list()
+
 f_p = sys.argv[1]
 prof_f = sys.argv[2]
 budgets = [0.1, 0.3, 0.5]
+t = time.time()
 g = load_instance(f_p)
+times.append(time.time() - t)
 
 def solve_for_multiple():
 	percentages = [0.1, 0.3, 0.5]
 	for p in percentages:
 		# print("percetage of total capacitance:" , p)
+		t = time.time()
 		print(grasp(Solution, [lambda x: x * p, f_p, g], Neighbourhood))
-
+		times.append(time.time() - t)
 
 cProfile.run('solve_for_multiple()', prof_f)
+print(times)
