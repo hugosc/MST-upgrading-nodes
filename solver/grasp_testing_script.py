65fc427fead75e44c70a853acb5ae0ae0689e7d9
Matheus Andrade <matheusgandrade@poli.ufrj.br>
2018-08-26 00:54:34 -0300

    change testing script to search for solutions given different budget ranges

--- solver/grasp_testing_script.py
+++ solver/grasp_testing_script.py
@@ -5,16 +5,9 @@ import graph_tool.all as gt
 from solution import Solution, Neighbourhood
 
 f_p = sys.argv[1]
-# print(sol.vertex_impact_ratio_on_tree())
 
 from grasp import grasp
-opt = grasp(Solution, [400, f_p], Neighbourhood)
-# print(sol.g.vp.is_upgraded.a)
-
-# graph = utils.load_instance(f_p)
-# print(graph.num_vertices())
-# tree_map = graph.new_edge_property("bool")
-
-# gt.min_spanning_tree(graph, weights=graph.ep.weight, tree_map=tree_map)
-
-# print(tree_map)
+percentages = [0.1, 0.3, 0.5, 0.7, 0.9, 1.0]
+for p in percentages:
+	print("percetage of total capacitance:" , p)
+	opt = grasp(Solution, [lambda x: x * p, f_p], Neighbourhood)
