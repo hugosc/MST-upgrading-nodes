72e7f73f740821e9e642c9d77203cc84d1518d72
Igor <igordeoliveiranunes@gmail.com>
2018-08-27 19:12:07 -0300

    change grasp to class

--- solver/grasp/__init__.py
+++ solver/grasp/__init__.py
@@ -3,59 +3,67 @@ __all__ = []
 import numpy as np
 from solution import first_improvement
 
-
-# Select candidates in the RCS style. In this case, we want to find vertices
-#  which maximizes the ratio "sol_improv / cost".
-#
-def select_candidate(sol, alpha):
-	candidates = sol.vertex_impact_ratio_on_tree()
-	ratio = candidates[:,0]
-	d = alpha * np.ptp(ratio)
-	c_max = np.max(ratio)
-
-	# random uniform sample of rcl
-	return np.random.permutation(candidates[ratio >= c_max - d])[0]
+class Grasp:
 
 
-def build(sol, alpha):
-	s = sol.copy()
-	s.cleanse_to_state()
-	while not s.is_saturated():
-		u = select_candidate(s, alpha)
-		s.upgrade_vertex(int(u[1]), update_mst=True)
-	return s
+	def __init__(self, solution, params, neigh, alpha = 0.4, max_it = 10):
+		self.solution = solution
+		self.params = params
+		self.neigh = neigh
+		self.alpha = alpha
+		self.max_it = max_it
 
 
+	# Select candidates in the RCS style. In this case, we want to find vertices
+	#  which maximizes the ratio "sol_improv / cost".
+	#
+	def select_candidate(self, sol):
+		candidates = sol.vertex_impact_ratio_on_tree()
+		ratio = candidates[:,0]
+		d = self.alpha * np.ptp(ratio)
+		c_max = np.max(ratio)
 
-def fimprov_local_search(sol, neigh):
-	"""BROKEN"""
-	print('initial obj_value: %f' % (sol.obj_value(),))
-	N = neigh(sol)
-	while first_improvement(N, 2):
-		print('improvement: %f' % (sol.obj_value(),))
-		
-	return sol
-
-
-# def iterated_local_search(sol, p_func, p_params, f_func, f_params, max_it=10):
-# 	n_it = 0
-# 	while n_it < max_it:
-# 		sol = 
+		# random uniform sample of rcl
+		return np.random.permutation(candidates[ratio >= c_max - d])[0]
 
 
+	def build(self, sol):
+		s = sol.copy()
+		s.cleanse_to_state()
+		while not s.is_saturated():
+			u = self.select_candidate(s)
+			s.upgrade_vertex(int(u[1]), update_mst=True)
+		return s
 
-def grasp(solution, params, neigh, alpha=0.4, max_it=10):
-	n_it = 0
-	sol = solution(*params)
-	opt =  sol
-	while n_it < max_it:
-		# print("Grasp iteration {}".format(n_it))
-		sol1 = build(sol, alpha)
 
-		fimprov_local_search(sol1, neigh)
+	def fimprov_local_search(self, sol):
+		"""BROKEN"""
+		print('initial obj_value: %f' % (sol.obj_value(),))
+		N = self.neigh(sol)
+		while first_improvement(N, 2):
+			print('improvement: %f' % (sol.obj_value(),))
+			
+		return sol
 
-		if opt._obj_value > sol1._obj_value:
-			opt = sol1
-		n_it += 1
-	print(opt)
-	return opt
+
+	# def iterated_local_search(sol, p_func, p_params, f_func, f_params, max_it=10):
+	# 	n_it = 0
+	# 	while n_it < max_it:
+	# 		sol = 
+
+
+	def run(self):
+		n_it = 0
+		self.solution = self.solution(*self.params)
+		opt =  self.solution
+		while n_it < self.max_it:
+			# print("Grasp iteration {}".format(n_it))
+			sol1 = self.build(self.solution)
+
+			self.fimprov_local_search(sol1)
+
+			if opt._obj_value > sol1.obj_value():
+				opt = sol1
+			n_it += 1
+		print(opt)
+		return opt
--- solver/grasp_testing_script.py
+++ solver/grasp_testing_script.py
@@ -6,8 +6,9 @@ from solution import Solution, Neighbourhood
 
 f_p = sys.argv[1]
 
-from grasp import grasp
+from grasp import Grasp
 percentages = [0.1, 0.3, 0.5]
 for p in percentages:
 	print("percetage of total capacitance:" , p)
-	opt = grasp(Solution, [lambda x: x * p, f_p], Neighbourhood, max_it=2)
+	grasp = Grasp(Solution, [lambda x: x * p, f_p], Neighbourhood, max_it=2)
+	opt = grasp.run()
